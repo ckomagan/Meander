308c865d6b80017843afaf3c67289bbd3cf9b162
Chan Komagan <chan@komagan.com>
2015-08-03 14:00:59 -0500

    Initial Commit

---
+++ Meander/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Meander
+//
+//  Created by Chan Komagan on 8/3/15.
+//  Copyright (c) 2015 Chan Komagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Meander/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Meander
+//
+//  Created by Chan Komagan on 8/3/15.
+//  Copyright (c) 2015 Chan Komagan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
